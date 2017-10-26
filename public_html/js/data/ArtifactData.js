155391bd417957cbe4a8fe359fc6af87f7e40d96
jacks0n <Rosty@DWPC.fritz.box>
2017-10-26 16:52:11 +0200

    Finalize artifact names

--- public_html/js/data/ArtifactData.js
+++ public_html/js/data/ArtifactData.js
@@ -4,25 +4,21 @@ if (!TT2) {
 
 TT2.Artifacts = [
     // S
-    // 20.49 effect x
     {tier: 'S', id: 1, name: 'Book of Shadows', maxLvl: 0, addupPerLvl: 0.0541388888888889, damPerLvl: 0.3, costCoef: 0.7, costExp: 2.5, offset: 1, effect: 'Prestige Relic', pref: '&#10005', app: ''},
     // A
     {tier: 'A', id: 2, name: 'Parchment of Foresight', maxLvl: 0, addupPerLvl: 0.2, damPerLvl: 0.4, costCoef: 0.6, costExp: 1.7, offset: 1, effect: 'War Cry Skill Effect', pref: '&#10005', app: ''},
     {tier: 'A', id: 3, name: 'Laborer\'s Pendant', maxLvl: 0, addupPerLvl: 0.2, damPerLvl: 0.4, costCoef: 0.6, costExp: 1.7, offset: 1, effect: 'Hand Of Midas Effect', pref: '&#10005', app: ''},
     {tier: 'A', id: 43, name: 'Invader\'s Gjalarhorn', maxLvl: 0, addupPerLvl: 0.02, damPerLvl: 0.2, costCoef: '1.2', costExp: 1.5, offset: 1, effect: 'All Active Skill Effect', pref: '&#10005', app: ''},
-    // 199.2 effect x
     {tier: 'A', id: 4, name: 'Heroic Shield', maxLvl: 0, addupPerLvl: 0.2477, damPerLvl: 0.5, costCoef: 0.7, costExp: 1.8, offset: 1, effect: 'Boss Gold', pref: '&#10005', app: ''},
     {tier: 'A', id: 5, name: 'Furies Bow', maxLvl: 0, addupPerLvl: 0.15, damPerLvl: 0.6, costCoef: 0.7, costExp: 1.7, offset: 1, effect: 'Ranged Hero Damage', pref: '&#10005', app: ''},
     {tier: 'A', id: 9, name: 'Hero\'s Blade', maxLvl: 0, addupPerLvl: 0.05, damPerLvl: 0.5, costCoef: 0.7, costExp: 1.7, offset: 1, effect: 'All Hero Damage', pref: '&#10005', app: ''},
     // B
     {tier: 'B', id: 10, name: 'Heavenly Sword', maxLvl: 0, addupPerLvl: 0.05, damPerLvl: 0.3, costCoef: 0.7, costExp: 2.2, offset: 1, effect: 'All artifact Damage', pref: '&#10005', app: ''},
-    // 44.76 effect
     {tier: 'B', id: 11, name: 'Divine Retribution', maxLvl: 0, addupPerLvl: 0.02, damPerLvl: 0.3, costCoef: '1.0', costExp: 2, offset: 1, effect: 'All Damage', pref: '&#10005', app: ''},
     {tier: 'B', id: 39, name: 'Influential Elixir', maxLvl: 0, addupPerLvl: 0.1, damPerLvl: 0.5, costCoef: 0.7, costExp: 1.7, offset: 1, effect: 'Clan Ship Damage', pref: '&#10005', app: ''},
     {tier: 'B', id: 17, name: 'Royal Toxin', maxLvl: 0, addupPerLvl: 0.1, damPerLvl: 0.5, costCoef: 0.7, costExp: 1.8, offset: 1, effect: 'Deadly Strike Effect', pref: '&#10005', app: ''},
     {tier: 'B', id: 49, name: 'Heart of Storms', maxLvl: 0, addupPerLvl: 0.025, damPerLvl: 0.3, costCoef: 0.6, costExp: 1.7, offset: 1, effect: 'Splash Damage', pref: '+', app: '%'},
     {tier: 'B', id: 50, name: 'Coins of Ebizu', maxLvl: 0, addupPerLvl: 0.025, damPerLvl: 0.3, costCoef: 0.7, costExp: 1.8, offset: 1, effect: 'Splash Gold', pref: '+', app: '%'},
-    // 65.8 effect
     {tier: 'B', id: 12, name: 'Book of Prophecy', maxLvl: 0, addupPerLvl: 0.05, damPerLvl: 0.3, costCoef: 0.7, costExp: 2.2, offset: 1, effect: 'All Gold', pref: '&#10005', app: ''},
     {tier: 'B', id: 18, name: 'Blade of Damocles', maxLvl: 0, addupPerLvl: 0.02, damPerLvl: 0.3, costCoef: 0.65, costExp: 2, offset: 1, effect: 'Sword Boost', pref: '&#10005', app: ''},
     {tier: 'B', id: 19, name: 'Helmet of Madness', maxLvl: 0, addupPerLvl: 0.02, damPerLvl: 0.3, costCoef: 0.65, costExp: 2, offset: 1, effect: 'Helmet Boost', pref: '&#10005', app: ''},
@@ -39,8 +35,7 @@ TT2.Artifacts = [
     {tier: 'D', id: 23, name: 'Staff of Radiance', maxLvl: 40, addupPerLvl: 2, damPerLvl: 0.8, costCoef: 0.5, costExp: 2.6, offset: 0, effect: 'Hero upgrade cost', pref: '-', app: '%'},
     {tier: 'D', id: 24, name: 'Invader\'s Shield', maxLvl: 50, addupPerLvl: 0.5, damPerLvl: 1.6, costCoef: 0.5, costExp: 2.1, offset: 0, effect: 'Double Fairy Chance', pref: '+', app: '%'},
     {tier: 'D', id: 13, name: 'Divine Chalice', maxLvl: 50, addupPerLvl: 1, damPerLvl: 0.8, costCoef: 0.8, costExp: 2.6, offset: 0, effect: 'Chance for 10x Gold', pref: '+', app: '%'},
-    {tier: 'D', id: 42, name: 'X', maxLvl: 30, addupPerLvl: 1, damPerLvl: 0.8, costCoef: 0.7, costExp: 1.8, offset: 0, effect: 'All Active Skill Duration', pref: '+', app: 's'},
-    // 26.46 effect
+    {tier: 'D', id: 42, name: 'Phantom Timepiece', maxLvl: 30, addupPerLvl: 1, damPerLvl: 0.8, costCoef: 0.7, costExp: 1.8, offset: 0, effect: 'All Active Skill Duration', pref: '+', app: 's'},
     {tier: 'D', id: 25, name: 'Chest of Contentment', maxLvl: 0, addupPerLvl: 0.25, damPerLvl: 0.4, costCoef: '1.0', costExp: 1.8, offset: 1, effect: 'Chesterson Gold', pref: '&#10005', app: ''},
     {tier: 'D', id: 26, name: 'Egg of Fortune', maxLvl: 10, addupPerLvl: 1, damPerLvl: 0.3, costCoef: 1.4, costExp: 3, offset: 0, effect: 'Chesterson Chance', pref: '+', app: '%'},
     {tier: 'D', id: 20, name: 'Axe of Muerte', maxLvl: 20, addupPerLvl: 0.5, damPerLvl: 3, costCoef: 0.8, costExp: 2.5, offset: 0, effect: 'Critical Strike Chance', pref: '+', app: '%'},
@@ -59,12 +54,9 @@ TT2.Artifacts = [
     {tier: 'E', id: 34, name: 'Elixir of Eden', maxLvl: 0, addupPerLvl: 0.2, damPerLvl: 0.4, costCoef: 0.6, costExp: 1.7, offset: 1, effect: 'Shadow Clone Effect', pref: '&#10005', app: ''},
     {tier: 'E', id: 35, name: 'Infinity Pendulum', maxLvl: 20, addupPerLvl: 1, damPerLvl: 0.9, costCoef: 0.6, costExp: 3, offset: 0, effect: 'Heavenly Strike Cost', pref: '-', app: 'm'},
     {tier: 'E', id: 36, name: 'Titan\'s Mask', maxLvl: 0, addupPerLvl: 0.2, damPerLvl: 0.4, costCoef: 0.6, costExp: 1.7, offset: 1, effect: 'Heavenly Strike Effect', pref: '&#10005', app: ''},
-    // 31.7 effect
     {tier: 'E', id: 38, name: 'Stone of the Valrunes', maxLvl: 0, addupPerLvl: 0.1, damPerLvl: 0.5, costCoef: 0.7, costExp: 1.8, offset: 1, effect: 'Monster Gold', pref: '&#10005', app: ''},
-    // new artifacts done
     {tier: 'E', id: 46, name: 'Zakinthos Coin', maxLvl: 0, addupPerLvl: 0.2, damPerLvl: 0.3, costCoef: 0.7, costExp: 1.8, offset: 1, effect: 'Inactive Gold', pref: '&#10005', app: ''},
     {tier: 'E', id: 47, name: 'The Master\'s Sword', maxLvl: 40, addupPerLvl: 2, damPerLvl: 2, costCoef: 0.7, costExp: 1.8, offset: 1, effect: 'Sword Master Upgrade Cost', pref: '-', app: '%'},
     {tier: 'E', id: 45, name: 'Avian Feather', maxLvl: 0, addupPerLvl: 0.2, damPerLvl: 0.3, costCoef: 0.6, costExp: 1.7, offset: 1, effect: 'Inactive Damage', pref: '&#10005', app: ''},
-    // new artifacts remaining
     {tier: 'E', id: 44, name: 'Samosek Sword', maxLvl: 0, addupPerLvl: 0.1, damPerLvl: 0.5, costCoef: 0.6, costExp: 2.0, offset: 1, effect: 'Sword  Attack Damage', pref: '&#10005', app: ''}
 ];
